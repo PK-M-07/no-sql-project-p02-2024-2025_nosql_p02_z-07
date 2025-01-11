23dbe1d9dfd6c708b1c0dc20080639284711468c
Bartek <Twj Email>
2025-01-11 17:21:49 +0100

    Project done !! maybe

--- RandomCook/main.py
+++ RandomCook/main.py
@@ -2,7 +2,3 @@ from appGUI import createMainWindow
 
 if __name__ == "__main__":
     createMainWindow()
-
-
-# TO - DO:
-# zrobić tak aby dla braku nic nie wyświetlało oprócz tych dwóch basic comboboxów
